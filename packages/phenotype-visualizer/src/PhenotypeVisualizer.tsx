a046b5791a68e2daa59d4c6f30b74ea269694d59
Luke Rasmussen <luke.rasmussen@gmail.com>
2020-09-18 17:24:38 -0500

    Use graphviz-react for AST viewing

--- packages/phenotype-visualizer/src/PhenotypeVisualizer.tsx
+++ packages/phenotype-visualizer/src/PhenotypeVisualizer.tsx
@@ -1,42 +1,18 @@
-import React from "react";
-
-import dot from "graphlib-dot";
-import DagreGraph from "dagre-d3-react";
-
+import React from 'react';
+import { Graphviz } from 'graphviz-react';
 import "./PhenotypeVisualizer.scss";
 
 const PhenotypeVisualizer: React.FC = ({ dotsrc }) => {
-  const graph = dot.read(dotsrc);
-
-  const nodes = Object.keys(graph._nodes).map((key) => {
-    return {
-      id: key,
-      label: graph._nodes[key].label,
-    };
-  });
-
-  const links = Object.keys(graph._edgeObjs).map((key) => {
-    return {
-      source: graph._edgeObjs[key].v,
-      target: graph._edgeObjs[key].w,
-    };
-  });
-
-  const config = {
-    rankdir: "TB",
-    align: "UR",
-    ranker: "tight-tree",
-  };
-
+  const options = {
+    fit: false,
+    scale: 1,
+    width: "100vw",
+    height: "100vh",
+    zoom: false
+  }
   return (
     <div className="phenotypeVisualizer">
-      <DagreGraph
-        width="100%"
-        height="100%"
-        nodes={nodes}
-        links={links}
-        config={config}
-      />
+      <Graphviz dot={dotsrc} options={options} />
     </div>
   );
 };
