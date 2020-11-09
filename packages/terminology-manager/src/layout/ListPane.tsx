63630b22cf77dc3867d253791aaa93b616f1cafe
Pascal Brandt <psbrandt@gmail.com>
2020-11-09 11:03:46 -0800

    📋 Add copy to clipboard feature.

--- packages/terminology-manager/src/layout/ListPane.tsx
+++ packages/terminology-manager/src/layout/ListPane.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import {
   Classes,
   Icon,
@@ -12,6 +12,8 @@ import {
 } from "@blueprintjs/core";
 
 import { R4 } from "@ahryman40k/ts-fhir-types";
+
+import { TerminologyToaster } from "../TerminologyToaster";
 
 interface NodeLabelProps {
   title: string;
@@ -53,11 +55,29 @@ const bundleToTreeNodes = (
         label: <NodeLabel title={entry.resource?.name} subtitle={subtitle} />,
         icon: "th",
         secondaryLabel: (
-          <Button
-            minimal
-            icon="trash"
-            onClick={() => removeResourceFromBundle(index)}
-          />
+          <div className="terminologyManager__listPane__node__actions">
+            <Tooltip content="Copy resource to clipboard">
+              <CopyToClipboard
+                text={JSON.stringify(entry.resource, " ", 2)}
+                onCopy={() => {
+                  TerminologyToaster.show({
+                    message: `${resourceType} ${entry.resource?.name} copied to clipboard`,
+                    intent: Intent.SUCCESS,
+                    icon: "tick",
+                  });
+                }}
+              >
+                <Button minimal icon="clipboard" />
+              </CopyToClipboard>
+            </Tooltip>
+            <Tooltip content="Remove from bundle">
+              <Button
+                minimal
+                icon="trash"
+                onClick={() => removeResourceFromBundle(index)}
+              />
+            </Tooltip>
+          </div>
         ),
       });
     }
