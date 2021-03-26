db707c4a3b9b9bf15884b81cc253195adc5fec02
Pascal Brandt <psbrandt@gmail.com>
2021-03-26 13:01:29 -0700

    👛 Fix repo fetch.

--- packages/phekb-api/src/PheKB.ts
+++ packages/phekb-api/src/PheKB.ts
@@ -394,35 +394,35 @@ class PheKB {
     // return Promise.resolve(this.sortByName(staticList));
 
     // Stub for testing
-    const phenotypes = [{ "nid": "1197", "vid": "3079", "type": "phenotype", "language": "und", "title": "bone scan utilization ", "uid": "14731", "status": "1", "created": "1556237435", "changed": "1556237598", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1197" }, { "nid": "1205", "vid": "3310", "type": "phenotype", "language": "und", "title": "Digital Rectal Exam", "uid": "14731", "status": "1", "created": "1557773569", "changed": "1570722034", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1205" }, { "nid": "1252", "vid": "3172", "type": "phenotype", "language": "und", "title": "eMERGE Phenotyping III case/control files collection", "uid": "14932", "status": "1", "created": "1561554121", "changed": "1561988992", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1252" }, { "nid": "1295", "vid": "3230", "type": "phenotype", "language": "und", "title": "elll BMI file re-uploads for dbGaP submission", "uid": "14932", "status": "1", "created": "1565721216", "changed": "1566999496", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1295" }, { "nid": "1376", "vid": "3361", "type": "phenotype", "language": "und", "title": "Pascal Test", "uid": "14954", "status": "1", "created": "1574206167", "changed": "1574206167", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1376" }, { "nid": "1388", "vid": "3376", "type": "phenotype", "language": "und", "title": "GWAS ICD & Meds Files", "uid": "14932", "status": "1", "created": "1575571091", "changed": "1575571106", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1388" }, { "nid": "1394", "vid": "3547", "type": "phenotype", "language": "und", "title": "ECG Traits Extracted from Electrocardiograms using Natural Language Processing", "uid": "15101", "status": "1", "created": "1576613506", "changed": "1590768222", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1394" }, { "nid": "1404", "vid": "3468", "type": "phenotype", "language": "und", "title": "Urinary Incontinence", "uid": "15022", "status": "1", "created": "1579148917", "changed": "1583776371", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1404" }, { "nid": "1417", "vid": "3512", "type": "phenotype", "language": "und", "title": "ACO (Asthma COPD Overlap)", "uid": "14464", "status": "1", "created": "1581435187", "changed": "1587414815", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1417" }, { "nid": "1429", "vid": "3457", "type": "phenotype", "language": "und", "title": "elll ICD/CPT Files Refresh for eMERGESeq", "uid": "14932", "status": "1", "created": "1582732961", "changed": "1582737975", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1429" }, { "nid": "1431", "vid": "3463", "type": "phenotype", "language": "und", "title": "Obsessive-compulsive disorder", "uid": "14523", "status": "1", "created": "1583602776", "changed": "1583602776", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1431" }, { "nid": "1451", "vid": "3502", "type": "phenotype", "language": "und", "title": "COVID-19 Counts for eMERGE", "uid": "15059", "status": "1", "created": "1585685239", "changed": "1586453756", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1451" }, { "nid": "1454", "vid": "3539", "type": "phenotype", "language": "und", "title": "Familial Hypercholesterolemia (NLP)", "uid": "14941", "status": "1", "created": "1586403252", "changed": "1590253686", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1454" }, { "nid": "1506", "vid": "3586", "type": "phenotype", "language": "und", "title": "Type 1 and type 2 Diabetes Mellitus", "uid": "15149", "status": "1", "created": "1596674145", "changed": "1596751985", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1506" }, { "nid": "1516", "vid": "3600", "type": "phenotype", "language": "und", "title": "Thrombotic Event", "uid": "14954", "status": "1", "created": "1614190022", "changed": "1614715035", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1516" }];
+    // const phenotypes = [{ "nid": "1197", "vid": "3079", "type": "phenotype", "language": "und", "title": "bone scan utilization ", "uid": "14731", "status": "1", "created": "1556237435", "changed": "1556237598", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1197" }, { "nid": "1205", "vid": "3310", "type": "phenotype", "language": "und", "title": "Digital Rectal Exam", "uid": "14731", "status": "1", "created": "1557773569", "changed": "1570722034", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1205" }, { "nid": "1252", "vid": "3172", "type": "phenotype", "language": "und", "title": "eMERGE Phenotyping III case/control files collection", "uid": "14932", "status": "1", "created": "1561554121", "changed": "1561988992", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1252" }, { "nid": "1295", "vid": "3230", "type": "phenotype", "language": "und", "title": "elll BMI file re-uploads for dbGaP submission", "uid": "14932", "status": "1", "created": "1565721216", "changed": "1566999496", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1295" }, { "nid": "1376", "vid": "3361", "type": "phenotype", "language": "und", "title": "Pascal Test", "uid": "14954", "status": "1", "created": "1574206167", "changed": "1574206167", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1376" }, { "nid": "1388", "vid": "3376", "type": "phenotype", "language": "und", "title": "GWAS ICD & Meds Files", "uid": "14932", "status": "1", "created": "1575571091", "changed": "1575571106", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1388" }, { "nid": "1394", "vid": "3547", "type": "phenotype", "language": "und", "title": "ECG Traits Extracted from Electrocardiograms using Natural Language Processing", "uid": "15101", "status": "1", "created": "1576613506", "changed": "1590768222", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1394" }, { "nid": "1404", "vid": "3468", "type": "phenotype", "language": "und", "title": "Urinary Incontinence", "uid": "15022", "status": "1", "created": "1579148917", "changed": "1583776371", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1404" }, { "nid": "1417", "vid": "3512", "type": "phenotype", "language": "und", "title": "ACO (Asthma COPD Overlap)", "uid": "14464", "status": "1", "created": "1581435187", "changed": "1587414815", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1417" }, { "nid": "1429", "vid": "3457", "type": "phenotype", "language": "und", "title": "elll ICD/CPT Files Refresh for eMERGESeq", "uid": "14932", "status": "1", "created": "1582732961", "changed": "1582737975", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1429" }, { "nid": "1431", "vid": "3463", "type": "phenotype", "language": "und", "title": "Obsessive-compulsive disorder", "uid": "14523", "status": "1", "created": "1583602776", "changed": "1583602776", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1431" }, { "nid": "1451", "vid": "3502", "type": "phenotype", "language": "und", "title": "COVID-19 Counts for eMERGE", "uid": "15059", "status": "1", "created": "1585685239", "changed": "1586453756", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1451" }, { "nid": "1454", "vid": "3539", "type": "phenotype", "language": "und", "title": "Familial Hypercholesterolemia (NLP)", "uid": "14941", "status": "1", "created": "1586403252", "changed": "1590253686", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1454" }, { "nid": "1506", "vid": "3586", "type": "phenotype", "language": "und", "title": "Type 1 and type 2 Diabetes Mellitus", "uid": "15149", "status": "1", "created": "1596674145", "changed": "1596751985", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1506" }, { "nid": "1516", "vid": "3600", "type": "phenotype", "language": "und", "title": "Thrombotic Event", "uid": "14954", "status": "1", "created": "1614190022", "changed": "1614715035", "comment": "2", "promote": "0", "sticky": "0", "tnid": "0", "translate": "0", "uri": "https://dev-phekb.pantheonsite.io/api/node/1516" }];
 
-    // const phenotypes = [];
+    const phenotypes = [];
 
-    // const pageLimit = maxPages || 1000;
+    const pageLimit = maxPages || 1000;
 
-    // let page = 0;
+    let page = 0;
 
-    // let results;
-    // do {
-    //   results = await fetch(
-    //     `${this.baseUrl}/node?parameters[type]=phenotype&options[sort]=nid&page=${page}`
-    //   )
-    //     .then(async (res) => {
-    //       if (res.status != 200) {
-    //         return Promise.reject(`${res.status}: ${await res.text()}`);
-    //       }
+    let results;
+    do {
+      results = await fetch(
+        `${this.baseUrl}/node?parameters[type]=phenotype&options[sort]=nid&page=${page}`
+      )
+        .then(async (res) => {
+          if (res.status != 200) {
+            return Promise.reject(`${res.status}: ${await res.text()}`);
+          }
 
-    //       return res.json();
-    //     })
-    //     .catch((err) => {
-    //       return Promise.reject(err);
-    //     });
+          return res.json();
+        })
+        .catch((err) => {
+          return Promise.reject(err);
+        });
 
-    //   if (results.length > 0) {
-    //     phenotypes.push(...results);
-    //     page++;
-    //   }
-    // } while (results.length > 0 && page < pageLimit);
+      if (results.length > 0) {
+        phenotypes.push(...results);
+        page++;
+      }
+    } while (results.length > 0 && page < pageLimit);
 
     return this.sortByName(phenotypes);
   }
